74034b464dc757e1ae1315be1845683071305846
Echefu Tobechukwu <tobex360@gmail.com>
2026-03-10 13:09:04 +0100

    added cors url

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 7000;
 const DB_URL = process.env.DB_URL;
 
 app.use(cors({
-    origin:['http://localhost:5173','https://your-frontend-url.vercel.app'],
+    origin:['https://lightspeed-red.vercel.app/'],
     credentials: true
 }));
 app.use(express.json());
